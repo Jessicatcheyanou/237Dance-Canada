f396124d06c216cf5fde398a8391c631c9305625
JessicaTcheyanou <jessicatcheyanou@gmail.com>
2026-04-25 13:09:20 -0400

    237Dance | update main and NavBar | Create styles.css

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function Navbar() {
   return (
-    <nav>
+    <nav style={{ padding: "1rem", background: "#111", color: "white" }}>
       <h2>Rhythm Dance School</h2>
       <div>
         <Link to="/">Home</Link>
--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,10 @@
-
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App";
-import "./index.css";
-
-const queryClient = new QueryClient();
+import App from "./App.jsx";
+import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <QueryClientProvider client={queryClient}>
-        <App />
-      </QueryClientProvider>
-    </BrowserRouter>
+    <App />
   </React.StrictMode>
 );
-
